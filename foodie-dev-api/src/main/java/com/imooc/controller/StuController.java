982e5a9a2b15b8902490a779e192cd39bbc84e8e
Max <kolxy@host.local>
2021-01-20 11:08:33 +0800

    service demo

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/StuController.java
@@ -0,0 +1,21 @@
+package com.imooc.controller;
+
+import com.imooc.service.StuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
+public class StuController {
+
+    @Autowired
+    private StuService stuService;
+
+    @GetMapping("/getStu")
+    public Object getStu(int id) {
+        return stuService.getStuInfo(id);
+    }
+
+}
