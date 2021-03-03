bb55e35d09e25faeac71a4565e22266bba2e43c0
Max <kolxy@qq.com>
2021-03-03 11:28:53 +0800

    4-4 用户注册 创建用户service

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -0,0 +1,35 @@
+package com.imooc.controller;
+
+import com.imooc.service.UserService;
+import com.imooc.utils.IMOOCJSONResult;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("passport")
+public class PassportController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/usernameIsExist")
+    public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
+        // 不能为空
+        if (StringUtils.isBlank(username)) {
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
+        }
+
+        // 是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return IMOOCJSONResult.errorMsg("用户名已经存在");
+        }
+
+        // 请求成功
+        return IMOOCJSONResult.ok();
+    }
+}
