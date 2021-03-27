269ba20b6e27b6fb1396442434d77bb771779898
Max <kolxy@qq.com>
2021-03-27 11:22:54 +0800

    4-16 日志监控service执行时间

---
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/aspect/ServiceLogAspect.java
@@ -0,0 +1,44 @@
+package com.imooc.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceLogAspect {
+
+    public static final Logger log = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    /**
+     * AOP通知：
+     * 1. 前置通知 方法调用之前执行
+     * 2. 后置通知 方法正常调用之后执行
+     * 3. 环绕通知 方法调用前后都执行
+     * 4. 异常通知 如果在方法调用中发生异常
+     * 5. 最终通知 方法调用之后执行
+     */
+
+    @Around("execution(* com.imooc.service.impl..*.*(..))")
+    public Object recordTimeLog(ProceedingJoinPoint jointPoint) throws Throwable {
+        log.info("====== 开始执行 {}.{} ======",
+                jointPoint.getTarget().getClass(),
+                jointPoint.getSignature().getName());
+        long begin = System.currentTimeMillis();
+        // 执行目标service
+        Object result = jointPoint.proceed();
+        long end = System.currentTimeMillis();
+        long takeTime = end - begin;
+        if (takeTime > 3000) {
+            log.error("====== 执行结束, 耗时: {}毫秒 ======", takeTime);
+        } else if (takeTime > 2000) {
+            log.warn("====== 执行结束, 耗时: {}毫秒 ======", takeTime);
+        } else {
+            log.info("====== 执行结束, 耗时: {}毫秒 ======", takeTime);
+        }
+        return result;
+    }
+}
