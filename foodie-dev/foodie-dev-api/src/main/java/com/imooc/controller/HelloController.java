f6ffcb40034801dea9c57af231ec653df92b28cf
Max <kolxy@qq.com>
2021-03-27 10:23:08 +0800

    4-14 log4j

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -7,9 +9,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 @RestController
 public class HelloController {
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public Object hello() {
+        logger.info("info");
+        logger.debug("debug");
+        logger.warn("warn");
+        logger.error("error");
         return "hello";
     }
 }
